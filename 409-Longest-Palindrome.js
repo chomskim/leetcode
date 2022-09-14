529dfd8904a71d64bae7799b813af691ad4d0727
chomskim <csknova@gmail.com>
2022-09-14 15:29:02 +0900

    fin 409

--- 409-Longest-Palindrome.js
+++ 409-Longest-Palindrome.js
@@ -11,4 +11,13 @@ const longestPalindrome = function (s) {
   })
   let oddExist = false
   let res = 0
+  Object.keys(freqMap).forEach((ch) => {
+    const freq = freqMap[ch]
+    const di = Math.floor(freq / 2)
+    res = res + di * 2
+    const re = freq % 2
+    if (re === 1) oddExist = true
+  })
+  if (oddExist) res += 1
+  return res
 }
